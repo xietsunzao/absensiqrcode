cc4048951ac9f2aca0625fe21955671fee7a2618
xietsunzao <lordshinjo22@gmail.com>
2020-08-02 13:25:06 +0700

    absensi

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -2,5 +2,5 @@
     <div class="pull-right hidden-xs">
         <b>Version</b> 2.3.0
     </div>
-    <strong> &copy; 2019 <a href="#">PT Karya Alam Mandiri</a>.</strong> All rights reserved.
+    <strong> &copy; 2019 <a href="#">Absensi Karyawan</a>.</strong> Template by Admin LTE.
 </footer>
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -2,9 +2,9 @@
     <!-- Logo -->
     <a href="#" class="logo">
         <!-- mini logo for sidebar mini 50x50 pixels -->
-        <span class="logo-mini"><b>H</b>G</span>
+        <span class="logo-mini"><b>A</b>K</span>
         <!-- logo for regular state and mobile devices -->
-        <span class="logo-lg"><b>HARD GUARD</b></span>
+        <span class="logo-lg"><b>Absesi Karyawan</b></span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top" role="navigation">
