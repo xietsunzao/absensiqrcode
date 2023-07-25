ad4a7dce5ab1ecfde5e2710a7724a4514b2a5bfd
xietsunzao <lordshinjo22@gmail.com>
2023-07-25 21:11:17 +0700

    refactor: :wrench: load helper auth from config

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -90,7 +90,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'form', 'combobox');
+$autoload['helper'] = array('url', 'form', 'combobox','auth');
 
 /*
 | -------------------------------------------------------------------
