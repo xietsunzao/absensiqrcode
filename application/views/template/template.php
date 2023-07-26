3587fc2f414d0a65fa0c999a1ec5745b80ddb4d7
xietsunzao <lordshinjo22@gmail.com>
2023-07-26 14:16:55 +0700

    refactor: :recycle: add method is_login from template for session  login

--- application/views/template/template.php
+++ application/views/template/template.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html>
+<?php is_login() ?>
 
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Absensi Karyawan</title>
+    <title>Absensi QRCode</title>
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <?php $this->load->view('template/css'); ?>
 </head>
